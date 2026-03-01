cfb8513fb0f2c623b5a4168ee7529f5eeb8143a1
Your Name <you@example.com>
2026-03-01 13:38:58 +0530

    ficing_stage_4

---
+++ frontend/eslint.config.ts
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+// cast the whole config to any
+const eslintConfig: any = {
+  ignorePatterns: ["dist"],
+  extends: [
+    js.configs.recommended,
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    browser: true,
+  },
+  plugins: {
+    "react-hooks": reactHooks,
+    "react-refresh": reactRefresh,
+    "@typescript-eslint": tsPlugin,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+};
+
+export default eslintConfig;
